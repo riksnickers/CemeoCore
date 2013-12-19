606b10c4aaae3af077887988169cd4e31010848e
Tychke <tycha.buekers@hotmail.com>
2013-12-19 15:57:14 +0100

    Views en controllers van Calendar en userprofile

---
+++ CeMeOCore/CeMeOCore/Controllers/CalendarController.cs
@@ -0,0 +1,106 @@
+﻿using CeMeOCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CeMeOCore.Controllers
+{
+    public class CalendarController : Controller
+    {
+        private CeMeoContext _db = new CeMeoContext();
+        //
+        // GET: /Calendar/
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Overview of all the calendars.";
+            var cal = _db.Calendars;
+            return View(cal);
+        }
+
+        //
+        // GET: /Calendar/Details/5
+        public ActionResult Details(int id)
+        {
+            var cal = _db.Calendars.Find(id);
+            return View(cal);
+        }
+
+        //
+        // GET: /Calendar/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Calendar/Create
+        [HttpPost]
+        public ActionResult Create(Calendar cal)
+        {
+            try
+            {
+                if (cal == null)
+                {
+                    throw new ArgumentNullException("item");
+                }
+                _db.Calendars.Add(cal);
+                _db.SaveChanges();
+                return View(cal);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Calendar/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Calendar/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Calendar cal)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Calendar/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Calendar/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Calendar cal)
+        {
+            try
+            {
+                _db.Calendars.Remove(cal);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CeMeOCore/CeMeOCore/Controllers/UserProfileController.cs
@@ -0,0 +1,106 @@
+﻿using CeMeOCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CeMeOCore.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        private CeMeoContext _db = new CeMeoContext();
+        //
+        // GET: /UserProfile/
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Overview of all the user.";
+            var model = _db.Users;
+            return View(model);
+        }
+
+        //
+        // GET: /UserProfile/Details/5
+        public ActionResult Details(int id)
+        {
+            var usr = _db.Users.Find(id);
+            return View(usr);
+        }
+
+        //
+        // GET: /UserProfile/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /UserProfile/Create
+        [HttpPost]
+        public ActionResult Create(UserProfile usr)
+        {
+            try
+            {
+                if (usr == null)
+                {
+                    throw new ArgumentNullException("item");
+                }
+                _db.Users.Add(usr);
+                _db.SaveChanges();
+                return View(usr);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /UserProfile/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /UserProfile/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /UserProfile/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /UserProfile/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, UserProfile usr)
+        {
+            try
+            {
+                _db.Users.Remove(usr);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
