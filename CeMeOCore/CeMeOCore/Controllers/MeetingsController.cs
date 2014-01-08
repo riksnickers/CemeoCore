8dfd6a1f3a994bc435d907a0f9726098a62d0269
Tychke <tycha.buekers@hotmail.com>
2014-01-08 10:11:57 +0100

    Appointments: 50% Calendar: 30% Location: 100% Meeting: 90% Room: 30% UserProfile: 30%

--- CeMeOCore/CeMeOCore/Controllers/MeetingsController.cs
+++ CeMeOCore/CeMeOCore/Controllers/MeetingsController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -30,7 +31,7 @@ namespace CeMeOCore.Controllers
 
         //
         // GET: /Meetings/Create
-        public ActionResult Create()
+        /*public ActionResult Create()
         {
             return View();
         }
@@ -102,6 +103,86 @@ namespace CeMeOCore.Controllers
             {
                 return View();
             }
+        }*/
+
+        // GET: /MeetingControllerTest/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /MeetingControllerTest/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MeetingID,MeetingDate,Location,State")] Meeting meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Meetings.Add(meeting);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(meeting);
+        }
+
+        // GET: /MeetingControllerTest/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Meeting meeting = _db.Meetings.Find(id);
+            if (meeting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(meeting);
+        }
+
+        // POST: /MeetingControllerTest/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MeetingID,MeetingDate,State")] Meeting meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(meeting).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(meeting);
+        }
+
+        // GET: /MeetingControllerTest/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Meeting meeting = _db.Meetings.Find(id);
+            if (meeting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(meeting);
+        }
+
+        // POST: /MeetingControllerTest/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Meeting meeting = _db.Meetings.Find(id);
+            _db.Meetings.Remove(meeting);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
