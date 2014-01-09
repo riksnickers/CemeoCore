73049b59063c1e35948ae0de657a990a83bed0a9
Tom Boffe <tom.boffe@gmail.com>
2014-01-09 09:10:40 +0100

    Implemented GET all locations, GET a specific Location. And corrected Other Methods parameters.

--- CeMeOCore/CeMeOCore/Controllers/LocationController.cs
+++ CeMeOCore/CeMeOCore/Controllers/LocationController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CeMeOCore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,6 +11,38 @@ namespace CeMeOCore.Controllers
     [Authorize]
     public class LocationController : ApiController
     {
+        private CeMeoContext _db = new CeMeoContext();
+        // GET api/Location
+        [AcceptVerbs("GET")]
+        public IEnumerable<Location> Get()
+        {
+            return _db.Locations;
+        }
 
+        // GET api/values/5
+        [AcceptVerbs("GET")]
+        public Location Get(int id)
+        {
+            return _db.Locations.Where(l => l.LocationID == id).First();
+        }
+
+        // POST api/values
+        [AcceptVerbs("POST")]
+        public void Post([FromBody]Location value)
+        {
+        }
+
+        // PUT api/values/5
+        [AcceptVerbs("PUT")]
+        public void Put(int id, [FromBody]Location value)
+        {
+        }
+
+        // DELETE api/values/5
+        [AcceptVerbs("DELETE")]
+        public void Delete(int id)
+        {
+            
+        }
     }
 }
