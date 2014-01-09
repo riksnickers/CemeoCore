468af4ac248514264d178242506ac18e4a9c38a1
Tychke <tycha.buekers@hotmail.com>
2014-01-09 16:08:04 +0100

    Ready for statistics implementatie

---
+++ CeMeOCore/CeMeOCore/Controllers/StatisticsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CeMeOCore.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        //
+        // GET: /Statistics/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Statistics/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
