0ee0abb0559074f43a18de27cded9f86cd3fe789
Tychke <tycha.buekers@hotmail.com>
2014-01-09 14:25:12 +0100

    Meetings and appointsments: searching, sorting and paging

--- CeMeOCore/CeMeOCore/Controllers/AppointmentController.cs
+++ CeMeOCore/CeMeOCore/Controllers/AppointmentController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace CeMeOCore.Controllers
 {
@@ -18,12 +19,57 @@ namespace CeMeOCore.Controllers
 
         //
         // GET: /Appointment/
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            ViewBag.Title = "Overview of all the appointments.";
-            var model = _db.Appointments;
-            return View(model);
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Description" : "";
+            ViewBag.DescriptionSortParm = sortOrder == "Description" ? "Description" : "Description";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "Date_desc" : "Date";
+            ViewBag.BeginTimeSortParm = sortOrder == "BeginTime" ? "BeginTime" : "BeginTime";
+            ViewBag.EndTimeSortParm = sortOrder == "EndTime" ? "EndTime" : "EndTime";
+            ViewBag.LocationSortParm = sortOrder == "Location" ? "Location" : "Location";
 
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+            var apps = from s in _db.Appointments select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                apps = apps.Where(s => s.Description.Contains(searchString.ToUpper()));
+            }
+
+            switch (sortOrder)
+            {
+                case "Description":
+                    apps = apps.OrderByDescending(s => s.Description);
+                    break;
+                case "Date":
+                    apps = apps.OrderBy(s => s.Date);
+                    break;
+                case "BeginTime":
+                    apps = apps.OrderByDescending(s => s.BeginTime);
+                    break;
+                case "EndTime":
+                    apps = apps.OrderByDescending(s => s.EndTime);
+                    break;
+                case "Location":
+                    apps = apps.OrderByDescending(s => s.Location);
+                    break;
+                default:
+                    apps = apps.OrderBy(s => s.Description);
+                    break;
+            }
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(apps.ToPagedList(pageNumber, pageSize));
         }
 
         //
--- CeMeOCore/CeMeOCore/Controllers/MeetingsController.cs
+++ CeMeOCore/CeMeOCore/Controllers/MeetingsController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace CeMeOCore.Controllers
 {
@@ -14,11 +15,69 @@ namespace CeMeOCore.Controllers
         private CeMeoContext _db = new CeMeoContext();
         //
         // GET: /Meetings/
-        public ActionResult Index()
+        /*blic ActionResult Index()
         {
             ViewBag.Title = "Overview of all the meetings.";
             var model = _db.Meetings;
             return View(model);
+        }*/
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            //Title of the page
+            ViewBag.Title = "Overview of all the Locations.";
+
+            //Sorting
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.CreatorSortParm = String.IsNullOrEmpty(sortOrder) ? "Creator" : "";
+            ViewBag.MeetingDateSortParm = sortOrder == "MeetingDate" ? "MeetingDate" : "MeetingDate";
+            ViewBag.LocationSortParm = sortOrder == "Location" ? "Location" : "Location";
+            ViewBag.StateSortParm = sortOrder == "State" ? "State" : "State";
+            //Paging
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+            //End paging
+
+            var mets = from s in _db.Meetings select s;
+
+            //Searching
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                mets = mets.Where(s => (s.Creator.LastName + " " + s.Creator.FirstName).Contains(searchString.ToUpper()));
+            }
+            //End searching
+
+            switch (sortOrder)
+            {
+                case "Name_desc":
+                    mets = mets.OrderByDescending(s => s.Creator.LastName + " " + s.Creator.FirstName);
+                    break;
+                case "Street":
+                    mets = mets.OrderBy(s => s.MeetingDate);
+                    break;
+                case "Number":
+                    mets = mets.OrderByDescending(s => s.Location.Name);
+                    break;
+                case "City":
+                    mets = mets.OrderByDescending(s => s.State);
+                    break;
+                default:
+                    mets = mets.OrderByDescending(s => s.Creator.LastName + " " + s.Creator.FirstName);
+                    break;
+            }
+
+            //Paging
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(mets.ToPagedList(pageNumber, pageSize));
+            //End sorting
         }
 
         //
